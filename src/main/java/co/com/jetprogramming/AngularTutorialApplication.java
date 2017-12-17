f33001091294f93c95fd8e408a9d638271c85883
jorgetovar <jorgetovar621@gmail.com>
2017-12-17 12:14:15 -0500

    Interceptors

--- src/main/java/co/com/jetprogramming/AngularTutorialApplication.java
+++ src/main/java/co/com/jetprogramming/AngularTutorialApplication.java
@@ -4,9 +4,13 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import co.com.jetprogramming.interceptors.TokenHandlerInterceptor;
 
 @SpringBootApplication
-public class AngularTutorialApplication {
+public class AngularTutorialApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AngularTutorialApplication.class, args);
@@ -15,6 +19,11 @@ public class AngularTutorialApplication {
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new TokenHandlerInterceptor());
 	}
 
 }
